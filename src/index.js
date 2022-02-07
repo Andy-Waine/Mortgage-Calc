b0b0f294a2966b2aa78c01866cf811076299d46e
A W <amwtablet@gmail.com>
2022-02-07 16:11:38 -0500

    issue fixed: eslint conflict, moved version from 8.8 to 7.0 per React docs

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { render } from '@testing-library/react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const CompoundInterest = () => {
+    return (
+      <>
+        <form>
+          <TextField label="Outlined" variant="outlined" />
+          <TextField label="Filled" variant="filled" />
+          <TextField label="Standard" variant="standard" />
+        </form>
+      </>
+    );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<CompoundInterest />, document.querySelector("#root"));
