16c61413ce00a2b5bb2efb37df9b3116a899445e
A W <amwtablet@gmail.com>
2022-02-07 20:35:13 -0500

    added inline CSS spacing, result useState, and result container

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const CompoundInterest = () => {
   const [downPayment, setDowmPayment] = useState();
   const [interestRate, setInterestRate] = useState();
   const [months, setMonths] = useState();
+  const [result, setResult] = useState();
 
   const calculate = () => {
     const result = (price - downPayment) * Math.pow(1 + interestRate, months)
@@ -17,41 +18,53 @@ const CompoundInterest = () => {
 
     return (
       <>
-        <form>
-          <TextField 
-          label="Price of Home" 
-          variant="outlined"
-          onChange={(e) => setPrincipal(e.target.value)}
-           />
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "40vh",
+          }}
+        >
 
-          <TextField 
-          label="Down Payment" 
-          variant="outlined"
-          onChange={(e) => setDownPayment(e.target.value)}
-           />
+          <form>
+            <TextField 
+            label="Price of Home" 
+            variant="outlined"
+            onChange={(e) => setPrincipal(e.target.value)}
+            />
 
-          <TextField 
-          label="Ineterest Rate (APR)" 
-          variant="outlined"
-          onChange={(e) => setInterestRate(e.target.value / 100)}
-           />
+            <TextField 
+            label="Down Payment" 
+            variant="outlined"
+            onChange={(e) => setDownPayment(e.target.value)}
+            />
 
-          <TextField 
-          label="Length of Mortgage (Years)" 
-          variant="outlined"
-          onChange={(e) => setMonths(e.target.value * 12)}
-           />
+            <TextField 
+            label="Ineterest Rate (APR)" 
+            variant="outlined"
+            onChange={(e) => setInterestRate(e.target.value / 100)}
+            />
 
-          <Button 
-            variant="contained" 
-            color="primary" 
-            onClick={() => {
-              calculate();
-            }}
-            >
-            Calculate
-          </Button>
-        </form>
+            <TextField 
+            label="Length of Mortgage (Years)" 
+            variant="outlined"
+            onChange={(e) => setMonths(e.target.value * 12)}
+            />
+            <br />
+            <Button 
+              variant="contained" 
+              color="primary" 
+              onClick={() => {
+                calculate();
+              }}
+              >
+              Calculate
+            </Button>
+            <br />
+            <div style={{ fontSize: "30px" }}>${result}</div>
+          </form>
+        </div>
       </>
     );
 };
