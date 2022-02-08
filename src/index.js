4bc829697daba4b3459232b87829e49ed54080b8
A W <amwtablet@gmail.com>
2022-02-07 19:24:00 -0500

    added buttons with useState() hooks

--- src/index.js
+++ src/index.js
@@ -7,12 +7,45 @@ import Button from '@material-ui/core/Button';
 import { render } from '@testing-library/react';
 
 const CompoundInterest = () => {
+  const [price, setPrice] = useState();
+  const [downPayment, setDowmPayment] = useState();
+  const [interestRate, setInterestRate] = useState();
+  const [months, setMonths] = useState();
+
+  const calculate = () => {
+    //formula goes here, replace null
+    const result = null;
+  };
     return (
       <>
         <form>
-          <TextField label="Outlined" variant="outlined" />
-          <TextField label="Filled" variant="filled" />
-          <TextField label="Standard" variant="standard" />
+          <TextField 
+          label="Price of Home" 
+          variant="outlined"
+          onChange={(e) => setPrincipal(e.target.value)}
+           />
+
+          <TextField 
+          label="Down Payment" 
+          variant="outlined"
+          onChange={(e) => setDownPayment(e.target.value)}
+           />
+
+          <TextField 
+          label="Ineterest Rate (APR)" 
+          variant="outlined"
+          onChange={(e) => setInterestRate(e.target.value / 100)}
+           />
+
+          <TextField 
+          label="Length of Mortgage (Years)" 
+          variant="outlined"
+          onChange={(e) => setMonths(e.target.value * 12)}
+           />
+
+          <Button variant="contained" color="primary" onClick={() => {}}>
+            Calculate
+          </Button>
         </form>
       </>
     );
