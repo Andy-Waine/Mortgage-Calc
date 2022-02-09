0450bbdf63bf12388dd6883a22e6bb5f12a3a456
A W <amwtablet@gmail.com>
2022-02-08 19:43:55 -0500

    insurance and HOA textfields added with hooks, calculator function refactor into three parts for readability

--- src/index.js
+++ src/index.js
@@ -7,14 +7,22 @@ import Button from "@material-ui/core/Button";
 const CompoundInterest = () => {
   const [price, setPrice] = useState();
   const [taxes, setTaxes] = useState();
+  const [insurance, setInsurance] = useState();
+  const [hoa, setHoa] = useState();
   const [downPayment, setDownPayment] = useState();
   const [interestRate, setInterestRate] = useState();
   const [months, setMonths] = useState();
   const [result, setResult] = useState();
 
   const calculate = () => {
-    const initialPayment = parseInt(taxes) + (interestRate * (price - downPayment)) / (1- (Math.pow((1 + interestRate), (-months))));
-    const result = initialPayment;
+    //principal and interest function
+    const paymentPI = (interestRate * (price - downPayment)) / (1- (Math.pow((1 + interestRate), (-months))));
+
+    //Taxes, Insurance, HOA Dues
+    const paymentTIA = parseInt(hoa) + parseInt(insurance) + parseInt(taxes)
+
+    //Final Result
+    const result = paymentPI + paymentTIA;
     setResult(result.toFixed(2));
   };
     //Note: The interestRate is divided by 1200 because it is divided 100 (percenttodecimal) and by 12 (annual to monthly APR)
@@ -41,7 +49,7 @@ const CompoundInterest = () => {
             />
 
             <TextField 
-            label="Down Payment" 
+            label="Down Payment ($)" 
             variant="outlined"
             type="number"
             onChange={(e) => setDownPayment(e.target.value)}
@@ -62,11 +70,26 @@ const CompoundInterest = () => {
             />
 
             <TextField 
-            label="Tax" 
+            label="Property Tax (Monthly)" 
             variant="outlined"
             type="number"
             onChange={(e) => setTaxes(e.target.value)}
             />
+
+            <TextField 
+            label="Insurance (Monthly)" 
+            variant="outlined"
+            type="number"
+            onChange={(e) => setInsurance(e.target.value)}
+            />
+
+            <TextField 
+            label="HOA Dues (Monthly)" 
+            variant="outlined"
+            type="number"
+            onChange={(e) => setHoa(e.target.value)}
+            />
+
             <br />
             <Button 
               variant="contained" 
